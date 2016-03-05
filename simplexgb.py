64c3b4d4aef9a4ba9f30b21d1aa38878c47067dd
olykos <Orestis.D.Lykouropoulos.17@dartmouth.edu>
2016-03-05 17:03:33 -0500

    Cleaned up code a little bit

--- simplexgb.py
+++ simplexgb.py
@@ -17,55 +17,58 @@ def load_data(data_file):
 	cols = df.columns.tolist()[1:]
 	df = df[cols]
 
-	# #replace missing values with -999
-	df.fillna(-999, inplace=True)
+	#replace missing values with large negative value (to encode the absence of the variable)
+	df.fillna(-9999, inplace=True)
 
 	#encode categorical features as integers
 	cat_columns = df.select_dtypes(['object']).columns
 	for c in cat_columns:
 		df[c] = df[c].astype('category')
-
-	df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes) #this may not be strictly necessary, since we already convert to category type first
+	df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
 
 	return id_col, df
 
+def write_results(pred, ids_test):
+	'''
+	Write results csv file. Each line is ID,probablity pair
+	'''
+	submission = open('results.csv','w')
+	submission.write('ID,PredictedProb\n')
 
+	for i in range(len(preds)):
+		submission.write(str(ids_test[i]) + "," + str(preds[i])+"\n")
+	submission.close()
 
 if __name__=="__main__":
 
+	if len(sys.argv) != 3:
+		print("Usage: python simplexgb.py training_csv_file, testing_csv_file")
+		sys.exit(1)
+
 	#load data
-	ids_train, df = load_data(sys.argv[1])
+	_, df = load_data(sys.argv[1])
 	ids_test, predict_df = load_data(sys.argv[2]) #data set to predict values for
 
-	traindf, testdf = train_test_split(df, test_size = 0.2)
+	#split training set 80/20 and train on 80% use 20% to evaluate
+	traindf, evaldf = train_test_split(df, test_size = 0.2)
 
 	features = list(traindf.columns[1:])
 	labels = traindf.columns[0]
-
 	train_DMatrix = xgb.DMatrix(traindf[features], traindf[labels])
-	test_DMatrix = xgb.DMatrix(testdf[features], testdf[labels]) #for validation/early stopping
-
-	predict_DMatrix = xgb.DMatrix(predict_df)
+	eval_DMatrix = xgb.DMatrix(evaldf[features], evaldf[labels]) #for validation/early stopping
 
 	#specify parameters
-	param = {'max_depth':10, 'eta':0.1, 'silent':1, 'objective':'reg:linear' }
+	param = {'max_depth':6, 'eta':0.1, 'subsample':1, 'min_child_weight':5, 
+	'col_sample_bytree':0.8, 'silent':1, 'objective':'reg:linear'}
 
-	#specify validations set to watch performance
-	watchlist  = [(test_DMatrix,'eval'), (train_DMatrix,'train')]
-	num_round = 70
-	bst = xgb.train(param, train_DMatrix, num_round, watchlist)
+	#specify validation set to watch performance
+	watchlist  = [(train_DMatrix,'train'), (eval_DMatrix,'eval')]
+	num_round = 200
+	bst = xgb.train(param, train_DMatrix, num_round, watchlist, early_stopping_rounds=10)
 
 	#prediction
+	predict_DMatrix = xgb.DMatrix(predict_df)
 	preds = bst.predict(predict_DMatrix)
+	write_results(preds, ids_test)
 
-	test_submission = open('results.csv','w')
-	test_submission.write('ID,PredictedProb\n')
-
-	for i in range(len(preds)):
-		test_submission.write(str(ids_test[i]) + "," + str(preds[i])+"\n")
-
-	test_submission.close()
-
-	# print preds
-	# print ('error=%f' % ( sum(1 for i in range(len(preds)) if int(preds[i]>0.5)!=labels[i]) /float(len(preds))))
-	bst.save_model('0001.model')
+	# bst.save_model('0001.model')
